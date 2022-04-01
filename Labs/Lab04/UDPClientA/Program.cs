a8c82f61d95ff707518103b4cdd6f01ded453183
Dat0309 <anhtheboy011@gmail.com>
2022-04-01 15:11:58 +0700

    Update lab 04

--- Labs/Lab04/UDPClientA/Program.cs
+++ Labs/Lab04/UDPClientA/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace UDPClientA
 {
@@ -6,7 +9,40 @@ namespace UDPClientA
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            UdpClient udpClient = new UdpClient();
+            try
+            {
+                udpClient.Connect("127.0.0.1", 11000);
+                string msg = string.Empty;
+                byte[] bytes;
+
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                while (true)
+                {
+                    Console.WriteLine(">>>INPUT: ");
+                    msg = Console.ReadLine();
+
+                    if (msg.Equals("exit", StringComparison.InvariantCultureIgnoreCase))
+                        break;
+
+                    bytes = Encoding.UTF8.GetBytes(msg);
+
+                    udpClient.Send(bytes, bytes.Length);
+
+                    bytes = udpClient.Receive(ref endPoint);
+                    msg = Encoding.UTF8.GetString(bytes);
+
+                    Console.WriteLine(">>>SERVER: "+msg);
+
+                }
+                udpClient.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ex" + ex);
+            }
+            Console.ReadKey();
         }
     }
 }
---
+++ Labs/Lab04/UDPServer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient udpClient = new UdpClient(11000);
+            IPEndPoint remote = new IPEndPoint(IPAddress.Any, 0);
+            Console.WriteLine("Waiting for a connection...");
+
+            while (true)
+            {
+                byte[] bytes = udpClient.Receive(ref remote);
+                string msg = Encoding.UTF8.GetString(bytes);
+                if (msg.Equals("exit all", StringComparison.InvariantCultureIgnoreCase))
+                    break;
+
+                Console.WriteLine(">>>CLIENT :"+msg);
+
+                Console.WriteLine(">>>INPUT :");
+                msg = Console.ReadLine();
+                bytes = Encoding.UTF8.GetBytes(msg);
+                udpClient.Send(bytes, bytes.Length, remote);
+            }
+            udpClient.Close();
+        }
+    }
+}
