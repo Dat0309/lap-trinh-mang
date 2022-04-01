993b2e46679d079ad847babfe12c2165d20a4e56
Dat0309 <anhtheboy011@gmail.com>
2022-04-01 14:17:40 +0700

    Update labs 4

---
+++ Labs/Lab04/Lab4_1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Lab4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient tcpClient = new TcpClient("127.0.0.1", 5000);
+                string msg = string.Empty;
+                byte[] buffer;
+
+                NetworkStream stream = tcpClient.GetStream();
+
+                while (true)
+                {
+                    Console.WriteLine(">>>INPUT: ");
+                    msg = Console.ReadLine();
+
+                    if (msg.Equals("exit", StringComparison.InvariantCultureIgnoreCase))
+                        break;
+
+                    buffer = Encoding.UTF8.GetBytes(msg);
+
+                    stream.Write(buffer, 0,buffer.Length);
+                    if (msg.Equals("exit all", StringComparison.InvariantCultureIgnoreCase))
+                        break;
+
+                    buffer = new byte[1024];
+
+                    string response = String.Empty;
+                    int bytes = stream.Read(buffer, 0, buffer.Length);
+                    response = Encoding.UTF8.GetString(buffer, 0, bytes);
+                    Console.WriteLine(">>>SERVER: " + response);
+                }
+                stream.Close();
+                tcpClient.Close();
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex);
+            }catch (SocketException ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Labs/Lab04/TCPListner/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCPListner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = null;
+            string data = string.Empty;
+            byte[] bytes;
+
+            try
+            {
+                IPAddress address = IPAddress.Parse("127.0.0.1");
+
+                server = new TcpListener(address, 5000);
+                server.Start();
+
+                bytes = new byte[1024];
+
+                Console.WriteLine("Waiting for a connection...");
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Connected successfully");
+                Console.WriteLine("Found client at {0}", client.Client.RemoteEndPoint.ToString());
+
+                NetworkStream stream = client.GetStream();
+
+                while (true)
+                {
+                    data = null;
+                    int i;
+                    while((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        data = Encoding.UTF8.GetString(bytes, 0, i);
+                        if (data.Equals("exit all", StringComparison.InvariantCultureIgnoreCase))
+                            break;
+                        Console.WriteLine(">>>CLIENT: " + data);
+
+                        Console.WriteLine(">>>INPUT: ");
+                        data = Console.ReadLine();
+                        byte[] msg = Encoding.UTF8.GetBytes(data);
+
+                        stream.Write(msg, 0, msg.Length);
+                    }
+                }
+                client.Close();
+            }catch (SocketException ex)
+            {
+                Console.WriteLine("SocketException: " + ex);
+            }
+            finally
+            {
+                server.Stop();
+            }
+        }
+    }
+}
